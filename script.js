a48d41707fd60834ab7049ed08f0bb6dff4c458f
gregocacao <131958237+gregocacao@users.noreply.github.com>
2025-08-30 14:52:10 +0200

    script.js

--- script.js
+++ script.js
@@ -165,7 +165,7 @@ function displayWinPage() {
             <h2>Bravo Champion !</h2>
             <img src="./images/photo_11.jpg" alt="Photo de victoire" class="quiz-image">
             <p class="garmin-link-container">
-                <a href="URL_DE_VOTRE_TRACE_GARMIN" target="_blank" class="garmin-link">Découvrez une trace Garmin spéciale !</a>
+                <a href="URL_DE_VOTRE_TRACE_GARMIN" target="_blank" class="garmin-link">Découvrez la trace Garmin !</a>
             </p>
         </div>
     `;
